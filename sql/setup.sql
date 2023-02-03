7e02abe4ecd26e242691ca470bef5c31588cfd6c
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-02-03 15:37:12 -0800

    add models and controllers for scores and inventories, add foreign key to users table

--- sql/setup.sql
+++ sql/setup.sql
@@ -20,3 +20,22 @@ CREATE TABLE studios (
   city VARCHAR,
   country VARCHAR
 );
+
+DROP TABLE IF EXISTS scores;
+
+CREATE TABLE scores (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT NOT NULL,
+  score INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+);
+
+DROP TABLE IF EXISTS inventories;
+
+CREATE TABLE inventories (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT NOT NULL,
+  coins INT,
+  skins VARCHAR,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+);
